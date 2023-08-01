bd8721da42c3e57bd94190f4455516863f5c6b22
krunaldavara <krunal.d@mol-tech.com>
2023-08-01 16:03:31 +0530

    Add project files.

---
+++ jenkinstesting/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace jenkinstesting.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
